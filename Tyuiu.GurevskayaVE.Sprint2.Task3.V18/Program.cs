2be92dacba2f63bffeb1b57ea83fd8a1273c9198
Tyuiu.GurevskayaVE@outlook.com <Tyuiu.GurevskayaVE@outlook.com>
2023-10-11 11:49:35 +0500

    task3

--- Tyuiu.GurevskayaVE.Sprint2.Task3.V18/Program.cs
+++ Tyuiu.GurevskayaVE.Sprint2.Task3.V18/Program.cs
@@ -11,9 +11,26 @@ namespace Tyuiu.GurevskayaVE.Sprint2.Task3.V18
     {
         static void Main(string[] args)
         {
+            Console.Title = "Спринт #2 | Выполнила: Гурьевская В.Е. | ПКТб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Вложенные операторы if - else                                     *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнила: Гурьевская В.Е. | ПКТб-23-2                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение Y с            *");
+            Console.WriteLine("* использование вложенных операторов if - else, где пользователь вводит   *");
+            Console.WriteLine("* значение переменной х с клавиатуры.                                     *");
+            Console.WriteLine("* Округлить до 3 знаков после запятой.                                    *");
+            Console.WriteLine("***************************************************************************");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
-            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* y = x**2(((x+1)/(x-1)))**2 (x>1)                                        *");
+            Console.WriteLine("* y = (x**2-cos(x)**2+10)/(x**2-sin(x)**2+12) (x=0)                       *");
+            Console.WriteLine("* y = (1+1/x**2)**x (-22<x<2)                                             *");
+            Console.WriteLine("* y = x+10x-(1/x) (x<-22)                                                 *");
             Console.WriteLine("***************************************************************************");
 
             DataService ds = new DataService();
