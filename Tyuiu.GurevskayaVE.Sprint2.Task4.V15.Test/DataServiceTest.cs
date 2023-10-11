2d89cfa09e8f75d5333aff37033736212f97c6b3
Tyuiu.GurevskayaVE@outlook.com <Tyuiu.GurevskayaVE@outlook.com>
2023-10-11 12:15:29 +0500

    task4

---
+++ Tyuiu.GurevskayaVE.Sprint2.Task4.V15.Test/DataServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GurevskayaVE.Sprint2.Task4.V15.Lib;
+
+namespace Tyuiu.GurevskayaVE.Sprint2.Task4.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckCalculate1()
+        {
+            DataService ds = new DataService();
+            double x = 4;
+            double y = 9;
+            double res = ds.Calculate(x, y);
+            double wait = 83.7;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void CheckCalculate2()
+        {
+            DataService ds = new DataService();
+            double x = 4;
+            double y = 576;
+            double res = ds.Calculate(x, y);
+            double wait = 0.286;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.GurevskayaVE.Sprint2.Task4.V15/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GurevskayaVE.Sprint2.Task4.V15.Lib;
+
+namespace Tyuiu.GurevskayaVE.Sprint2.Task4.V15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнила: Гурьевская В.Е. | ПКТб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Тернарный оператор                                                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #15                                                             *");
+            Console.WriteLine("* Выполнила: Гурьевская В.Е. | ПКТб-23-2                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение с              *");
+            Console.WriteLine("* использованием тернарных операторов, где пользователь вводит            *");
+            Console.WriteLine("* значение переменной х, y с клавиатуры.                                  *");
+            Console.WriteLine("* Округлить до 3 знаков после запятой.                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("* если sqrt(x)/2 > sqrt(y)-20, то z = (3+2/y**2)**x                       *");
+            Console.WriteLine("* иначе cos(y)+(12/x**2)                                                  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            Console.Write("Введите х: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Значение функции: " + ds.Calculate(x, y));
+
+            Console.ReadKey();
+        }
+    }
+}
